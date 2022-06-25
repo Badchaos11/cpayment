31b38a4a235cd1ea04d76e7e8a7f5f636964eecc
Badchaos11 <undrk95@gmail.com>
2022-06-25 20:19:16 +0700

    print new string

--- handlers/get.go
+++ handlers/get.go
@@ -48,7 +48,7 @@ func (t *Transactions) GetAllByEmail(w http.ResponseWriter, r *http.Request) {
 	email := vars["email"]
 	trs := models.AllTransactionsEm(email) // запрос к БД
 	for i := 0; i < len(trs); i++ {        // вывод всех результатов в цикле
-		t.l.Println(trs[i])                                 // вывод результата в консоль
-		fmt.Fprint(w, "Данные транзакции: ", trs[i], " \n") // вывод результата в ответ на запрос
+		t.l.Println(trs[i])                                  // вывод результата в консоль
+		fmt.Fprint(w, "Данные транзакции: ", trs[i], "\r\n") // вывод результата в ответ на запрос
 	}
 }
